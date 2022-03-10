e918aa34eabac1ac1926ee25594a36956be3495d
Kyle Finkernagel <Kyle.Finkernagel@gmail.com>
2022-03-09 22:04:03 -0600

    point buy
    standard is not point buy ha ha

--- src/components/pointbuy/pointbuy.js
+++ src/components/pointbuy/pointbuy.js
@@ -1,13 +1,49 @@
 import {useState} from 'react'
 
 function Pointbuy() {
+    // 08 09 10 11 12 13 14 15 16 17 18 19 20   stat
+    // -1 -1  0  0  1  1  2  2  3  3  4  4  5   bonus
+    //  0  1  2  3  4  5  7  9					cost
+	const cost = [0, 1, 2, 3, 4, 5, 7, 9]
+	const STAT_FLOOR = 8
+	const STAT_CELING= 15
+
+    const [base_str, set_str] = useState(8)
+	const increaseSTR = () => {set_str((prev) => { return prev + 1 > STAT_CELING ? STAT_CELING : prev + 1 })}
+	const decSTR = () => {set_str((prev) => { return prev - 1 < STAT_FLOOR ? STAT_FLOOR : prev - 1 })}
+
+	const [base_dex, set_dex] = useState(8)
+	const increaseDEX = () => {set_dex((prev) => { return prev + 1 > STAT_CELING ? STAT_CELING : prev + 1 })} 
+	const decDEX = () => {set_dex((prev) => { return prev - 1 < STAT_FLOOR ? STAT_FLOOR : prev - 1 })}
+	
+	const [base_con, set_con] = useState(8)
+	const increaseCON = () => {set_con((prev) => { return prev + 1 > STAT_CELING ? STAT_CELING : prev + 1 })} 
+	const decCON = () => {set_con((prev) => { return prev - 1 < STAT_FLOOR ? STAT_FLOOR : prev - 1 })}
+
+	const [base_int, set_int] = useState(8)
+	const increaseINT = () => {set_int((prev) => { return prev + 1 > STAT_CELING ? STAT_CELING : prev + 1 })} 
+	const decINT = () => {set_int((prev) => { return prev - 1 < STAT_FLOOR ? STAT_FLOOR : prev - 1 })}
+		
+	const [base_wis, set_wis] = useState(8)
+	const increaseWIS = () => {set_wis((prev) => { return prev + 1 > STAT_CELING ? STAT_CELING : prev + 1 })} 
+	const decWIS = () => {set_wis((prev) => { return prev - 1 < STAT_FLOOR ? STAT_FLOOR : prev - 1 })}
+
+	const [base_cha, set_cha] = useState(8)
+	const increaseCHA = () => {set_cha((prev) => { return prev + 1 > STAT_CELING ? STAT_CELING : prev + 1 })} 
+	const decCHA = () => {set_cha((prev) => { return prev - 1 < STAT_FLOOR ? STAT_FLOOR : prev - 1 })}
+
 	return (
 		<div className='POINTBUY'>
 			<div className='POINTBUY_HEADER'>
-				POINT BUY HEADER
+				POINT BUY: 
 			</div>
 			<div className='POINTBUY_BODY'>
-				POINT BUY BODY
+				<span> STR: {base_str} COST: {cost[base_str-STAT_FLOOR]} </span><button onClick={increaseSTR}>+</button><button onClick={decSTR}>-</button><br/>
+				<span> DEX: {base_dex} COST: {cost[base_dex-STAT_FLOOR]} </span><button onClick={increaseDEX}>+</button><button onClick={decDEX}>-</button><br/>
+				<span> CON: {base_con} COST: {cost[base_con-STAT_FLOOR]} </span><button onClick={increaseCON}>+</button><button onClick={decCON}>-</button><br/>
+				<span> INT: {base_int} COST: {cost[base_int-STAT_FLOOR]} </span><button onClick={increaseINT}>+</button><button onClick={decINT}>-</button><br/>
+				<span> WIS: {base_wis} COST: {cost[base_wis-STAT_FLOOR]} </span><button onClick={increaseWIS}>+</button><button onClick={decWIS}>-</button><br/>
+				<span> CHA: {base_cha} COST: {cost[base_cha-STAT_FLOOR]} </span><button onClick={increaseCHA}>+</button><button onClick={decCHA}>-</button><br/>
 			</div>
 			<div className='POINTBUY_FOOTER'>
 				POINT BUY FOOTER
--- src/components/standard/standard.js
+++ src/components/standard/standard.js
@@ -1,45 +1,16 @@
 import {useState} from 'react'
 
 function Standard() {
-    const [str, set_str] = useState(8)
-	const increaseSTR = () => {set_str((prev) => { return prev + 1 > 20 ? 20 : prev + 1 })}
-	const decSTR = () => {set_str((prev) => { return prev - 1 < 8 ? 8 : prev - 1 })}
-
-	const [dex, set_dex] = useState(8)
-	const increaseDEX = () => {set_dex((prev) => { return prev + 1 > 20 ? 20 : prev + 1 })} 
-	const decDEX = () => {set_dex((prev) => { return prev - 1 < 8 ? 8 : prev - 1 })}
-	
-	const [con, set_con] = useState(8)
-	const increaseCON = () => {set_con((prev) => { return prev + 1 > 20 ? 20 : prev + 1 })} 
-	const decCON = () => {set_con((prev) => { return prev - 1 < 8 ? 8 : prev - 1 })}
-
-	const [int, set_int] = useState(8)
-	const increaseINT = () => {set_int((prev) => { return prev + 1 > 20 ? 20 : prev + 1 })} 
-	const decINT = () => {set_int((prev) => { return prev - 1 < 8 ? 8 : prev - 1 })}
-		
-	const [wis, set_wis] = useState(8)
-	const increaseWIS = () => {set_wis((prev) => { return prev + 1 > 20 ? 20 : prev + 1 })} 
-	const decWIS = () => {set_wis((prev) => { return prev - 1 < 8 ? 8 : prev - 1 })}
-
-	const [cha, set_cha] = useState(8)
-	const increaseCHA = () => {set_cha((prev) => { return prev + 1 > 20 ? 20 : prev + 1 })} 
-	const decCHA = () => {set_cha((prev) => { return prev - 1 < 8 ? 8 : prev - 1 })}
-
     return (
         <div className='STANDARD'>
             <div className='STANDARD_HEADER'>
-                STANDARD POINT BUY HEADER
+                STANDARD HEADER
             </div>
             <div className='STANDARD_BODY'>
-                <span> STR:  {str} </span><button onClick={increaseSTR}>+</button><button onClick={decSTR}>-</button><br/>
-				<span> DEX:  {dex} </span><button onClick={increaseDEX}>+</button><button onClick={decDEX}>-</button><br/>
-				<span> CON:  {con} </span><button onClick={increaseCON}>+</button><button onClick={decCON}>-</button><br/>
-				<span> INT:  {int} </span><button onClick={increaseINT}>+</button><button onClick={decINT}>-</button><br/>
-				<span> WIS:  {wis} </span><button onClick={increaseWIS}>+</button><button onClick={decWIS}>-</button><br/>
-				<span> CHA:  {cha} </span><button onClick={increaseCHA}>+</button><button onClick={decCHA}>-</button><br/>
+                STANDARD BODY
             </div>
             <div className='STANDARD_FOOTER'>
-                STANDARD POINT BUY FOOTER
+                STANDARD FOOTER
             </div>
         </div>
     )
